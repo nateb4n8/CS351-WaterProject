252da407b9f2058eb2041bd42378606c885b12a9
Robert Trujillo <rtru6727@unm.edu>
2013-04-23 11:31:14 -0600

    Plant and Crops Version 1 finalized
    This commit should have all the methods needed to add  a crop of plants
    to a farm quadrant and allow water flow to be calculated to and from the
    crop by the waterflow class.

--- src/cell/Crops.java
+++ src/cell/Crops.java
@@ -1,73 +1,112 @@
 package cell;
 
 /**
- * Author: Robert Trujillo
- * Date: 04.23.13
- * A Class used for the WaterFlow Gameplay so the Plant may interact with the user/farm
+ * Author: Robert Trujillo 
+ * Date: 04.23.13 
+ * A Crops class to create a crop on a quadrant of a farm.
  */
-public class Crops 
-{
-  
-  private Plant _plant; //holds crop plant type
-  private Farm _farm;//array of Cells where the crop is located 
-  private int _quadrant;//Quandrant number where Farm may be plotted numbers 0-3 are valid (0-> NW, 1-> NE, 2-> SW, 3-> SE) 
-  private int _weeksWithoutSufficentWater; //counter for weeks without water  
-  private int _cropSize; //variable to keep cropsize in number of plants
-  
-  
-  /**
-   * Sets the constant values of a Plant type.
-   * @param plantType type of plant for crop
-   * @param cropLocation in array of cells within farm
-   * @param cropSize number of plants in crop
-   * @throws Exception 
-   */
-  private Crops (Plant plant,Farm farm, int quadrant) throws Exception
-  {
-    
-	if(quadrant < 4){
-		this._quadrant = quadrant;
+public class Crops {
+
+	private Plant _plant; // holds crop plant type
+	private Farm _farm;// array of Cells where the crop is located
+	private int _quadrant;// Quandrant number where Farm may be plotted numbers(0-> NW, 1-> NE, 2-> SW, 3-> SE)
+	private int _cropSize; // variable to keep cropsize in number of plants
+
+	/**
+	 * Sets the constant values of a Plant type.
+	 * 
+	 * @param plantType
+	 *            type of plant for crop
+	 * @param cropLocation
+	 *            in array of cells within farm
+	 * @param cropSize
+	 *            number of plants in crop
+	 * @throws Exception
+	 */
+	private Crops(Plant plant, Farm farm, int quadrant) throws Exception {
+		// check if valid Crop entered
+		if (quadrant < 4) {
+			this._quadrant = quadrant;
+		} else
+			throw new Exception("Invalid Quadrant Entered");
+
+		this._plant = plant;
+		this._farm = farm;
+		this._cropSize = 0;
+
+		// plant seeds by quadrant, size currently set at 150 which is not
+		// divisible by 4 so hardcoded 75 for some quads
+		if (_quadrant == 0) {
+			for (int x = 0; x < 75; x += _plant.getDistanceBetweenSeeds()) {
+				for (int y = 0; y < 75; y += _plant.getDistanceBetweenSeeds()) {
+
+					_farm.getGrid()[x][y][_plant.getDepthOfSeed()]
+							.setPlant(_plant);
+					_cropSize++;
+				}
+			}
+		} else if (_quadrant == 1) {
+			for (int x = 75; x < _farm.SIZE; x += _plant
+					.getDistanceBetweenSeeds()) {
+				for (int y = 0; y < 75; y += _plant.getDistanceBetweenSeeds()) {
+
+					_farm.getGrid()[x][y][_plant.getDepthOfSeed()]
+							.setPlant(_plant);
+					_cropSize++;
+				}
+			}
+		} else if (_quadrant == 2) {
+			for (int x = 0; x < 75; x += _plant.getDistanceBetweenSeeds()) {
+				for (int y = 75; y < _farm.SIZE; y += _plant
+						.getDistanceBetweenSeeds()) {
+
+					_farm.getGrid()[x][y][_plant.getDepthOfSeed()]
+							.setPlant(_plant);
+					_cropSize++;
+				}
+			}
+		} else {
+			for (int x = 75; x < _farm.SIZE; x += _plant
+					.getDistanceBetweenSeeds()) {
+				for (int y = 75; y < _farm.SIZE; y += _plant
+						.getDistanceBetweenSeeds()) {
+
+					_farm.getGrid()[x][y][_plant.getDepthOfSeed()]
+							.setPlant(_plant);
+					_cropSize++;
+				}
+			}
+		}
 	}
-	else throw new Exception("Invalid Quadrant Entered");
-	  
-    this._plant = plant;
-    this._farm = _farm;
-    this._cropSize = 0;
-    
-    //plant seeds
-    for(int x = 0; x<= _farm.SIZE; x+= _plant.getDistanceBetweenSeeds()){
-    	for(int y = 0; y<= _farm.SIZE;y+=_plant.getDistanceBetweenSeeds()){
-    		
-    		_farm.getGrid()[x][y][_plant.getDepthOfSeed()].setPlant(_plant); 
-    		_cropSize++;
-    	}
-    }
-    
-    this._weeksWithoutSufficentWater = 0;//initially set to zero
-  }
-  
-  /**
-   * @return Returns the PlantType of this crop
-   */
-  public Plant getPlant()
-  { return this._plant;
-  }
-  
-  /**
-   * @return Returns the array of cells that this crop covers.
-   */
-  public Farm getFarm()
-  
-  { return this._farm;
-  }  
-  
-  /**
-   * @return Returns the CropSize in number of plants for this Crop
-   */
-  public double getCropSize()
-  { return this._cropSize;
-  }
-  
- 
-  
+
+	/**
+	 * @return Returns the PlantType of this crop
+	 */
+	public Plant getPlant() {
+		return this._plant;
+	}
+
+	/**
+	 * @return Returns the array of cells that this crop covers.
+	 */
+	public Farm getFarm()
+
+	{
+		return this._farm;
+	}
+
+	/**
+	 * @return Returns the CropSize in number of plants for this Crop
+	 */
+	public double getCropSize() {
+		return this._cropSize;
+	}
+
+	/**
+	 * @return Returns the quadrant of the farm that the crop is in
+	 **/
+	public int get_quadrant() {
+		return _quadrant;
+	}
+
 }
--- src/cell/Plant.java
+++ src/cell/Plant.java
@@ -1,90 +1,115 @@
 package cell;
 
 /**
- * Author: Robert Trujillo & Nathan Acosta
- * Date: Mar 20, 2013 , Modified by Robert on 04.22.13
+ * Author: Robert Trujillo & Nathan Acosta 
+ * Date: Created by Natehan on Mar 20, 2013, Modified by Robert on 04.22.13 
  * A Plant contains properties pertaining to its interaction
  * with water and physical structure.
  */
-public enum Plant
-{   	
-	PINTOBEANS(2.5, 3, 114, 11, 4, 2.65),	    
-	SUNFLOWER(3.2, 13, 25, 30, 12, 2.65),
-	AMARANTH(1.4, 2, 18, 4, 2, 1.33),
-	CHILE(7.2, 17, 61, 5, 1, 2.65),
-	SWEETCORN(1.65, 11, 46, 20, 3, 3.98),
-	SUMMERSQUASH(3.5, 7, 92, 45, 3, 2.65),
-	WINTERSQUASH(3.5, 16, 122, 120, 3, 2.65),
-	POTATOES(4.2, 17, 46, 30, 15, 2.65),
-	SWEETPEPPER(8.2, 80, 31, 3, 1, 2.65);
+public enum Plant {
+	// Plant Types and Attributes
+	PINTOBEANS(2.5, 3, 114, 11, 4, 2.65), SUNFLOWER(3.2, 13, 25, 30, 12, 2.65), AMARANTH(
+			1.4, 2, 18, 4, 2, 1.33), CHILE(7.2, 17, 61, 5, 1, 2.65), SWEETCORN(
+			1.65, 11, 46, 20, 3, 3.98), SUMMERSQUASH(3.5, 7, 92, 45, 3, 2.65), WINTERSQUASH(
+			3.5, 16, 122, 120, 3, 2.65), POTATOES(4.2, 17, 46, 30, 15, 2.65), SWEETPEPPER(
+			8.2, 80, 31, 3, 1, 2.65);
 
-	private double _transpiration; 
+	private double _transpiration;
 	private double _maturationTime;
-	private int _matureDepth; 
+	private int _matureDepth;
 	private int _distanceBetweenSeeds;
-	private int _depthOfSeed; 
+	private int _depthOfSeed;
+	private int _rootDepth;
 	private double _waterRequirements;
-  
-  /**
-   * Sets the constant values of a Plant type.
-   * @param transpiration in mL/Week
-   * @param maturationTime in Weeks
-   * @param matureDepth in cells
-   * @param distanceBetweenSeeds in cells
-   * @param depthOfSeed in cells
-   * @param waterRequirements in mL/week
-   */
-	private Plant (double transpiration, double maturationTime, int matureDepth, int distanceBetweenSeeds, int depthOfSeed, double waterRequirements)
-	{
+
+	/**
+	 * Sets the constant values of a Plant type.
+	 * 
+	 * @param transpiration
+	 *            in mL/Week
+	 * @param maturationTime
+	 *            in Weeks
+	 * @param matureDepth
+	 *            in cells
+	 * @param distanceBetweenSeeds
+	 *            in cells
+	 * @param depthOfSeed
+	 *            in cells
+	 * @param waterRequirements
+	 *            in mL/week
+	 */
+	private Plant(double transpiration, double maturationTime, int matureDepth,
+			int distanceBetweenSeeds, int depthOfSeed, double waterRequirements) {
 		this._transpiration = transpiration;
 		this._maturationTime = maturationTime;
 		this._matureDepth = matureDepth;
 		this._distanceBetweenSeeds = distanceBetweenSeeds;
-		this._depthOfSeed = depthOfSeed;    
+		this._depthOfSeed = depthOfSeed;
 		this._waterRequirements = waterRequirements;
+		this._rootDepth = 0;
 	}
-  
-  /**
-   * @return Returns the Transpiration of this Plant in mL/Week.
-   */
-  public double getTranspiration()
-  { return this._transpiration;
-  }
-  
-  /**
-   * @return Returns the Maturation Time of this Plant in Weeks.
-   */
-  public double getMaturationTimee()
-  { return this._maturationTime;
-  }
 
-  /**
-   * @return Returns the Mature Root Depth of this Plant in number of cells.
-   */
-  public int getMatureDepth()
-  { return this._matureDepth;
-  }
-  
-  /**
-   * @return Returns the Distance needed between seeds of this Plant in number of cells.
-   */
-  public int getDistanceBetweenSeeds()
-  { return this._distanceBetweenSeeds;
-  }
-  
-  /**
-   * @return Returns the Depth needed for the seed of this Plant in number of cells.
-   */
-  public int getDepthOfSeed()
-  { return this._depthOfSeed;
-  }
-  
-  /**
-   * @return Returns the Water Requirements of this Plant in mL/week.
-   */
-  public double getWaterRequirements()
-  { return this._waterRequirements;
-  
-  }
-  
+	/**
+	 * @return Returns the Transpiration of this Plant in mL/Week.
+	 */
+	public double getTranspiration() {
+		return this._transpiration;
+	}
+
+	/**
+	 * @return Returns the Maturation Time of this Plant in Weeks.
+	 */
+	public double getMaturationTimee() {
+		return this._maturationTime;
+	}
+
+	/**
+	 * @return Returns the Mature Root Depth of this Plant in number of cells.
+	 */
+	public int getMatureDepth() {
+		return this._matureDepth;
+	}
+
+	/**
+	 * @return Returns the Distance needed between seeds of this Plant in number
+	 *         of cells.
+	 */
+	public int getDistanceBetweenSeeds() {
+		return this._distanceBetweenSeeds;
+	}
+
+	/**
+	 * @return Returns the Depth needed for the seed of this Plant in number of
+	 *         cells.
+	 */
+	public int getDepthOfSeed() {
+		return this._depthOfSeed;
+	}
+
+	/**
+	 * @return Returns the Water Requirements of this Plant in mL/week.
+	 */
+	public double getWaterRequirements() {
+		return this._waterRequirements;
+
+	}
+
+	/**
+	 * @return Returns the Water Requirements of this Plant in mL/week.
+	 */
+	public int getRootDepth() {
+		return _rootDepth;
+	}
+
+	/**
+	 * @Param rootDepth sets rootDepth of plant in cm rootDepth must be less
+	 *        than or equal to matureDepth
+	 * @return Returns the Water Requirements of this Plant in mL/week.
+	 */
+	public void setRootDepth(int rootDepth) {
+		if (rootDepth <= this._matureDepth) {
+			this._rootDepth = rootDepth;
+		}
+	}
+
 }
