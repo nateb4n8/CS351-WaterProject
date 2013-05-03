bff1c9ce445bbcfb9525421d49df7073a6c022a5
Max <mottese@unm.edu>
2013-05-03 13:35:25 -0600

    Added Water Flow type to Network Data

--- src/server/NetworkData.java
+++ src/server/NetworkData.java
@@ -4,6 +4,6 @@ import java.io.Serializable;
 
 public abstract class NetworkData implements Serializable
 {
-  public enum Type {Name, Msg, BuyWater, Quit};//SellWater, CancelOrder, SellCrop
+  public enum Type {Name, Msg, BuyWater, Quit, FlowWater};//SellWater, CancelOrder, SellCrop
   public Type type;
 }
